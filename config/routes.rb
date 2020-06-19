ead0c3798dc37ed0758b01037464fb3d0d17ea1f
Tanner Noble <tannernoble@gmail.com>
2020-06-19 16:05:01 -0500

    Initialize repo with sb-admin-2 theme, devise, and devise user model.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
+  get 'pages/my_rebus_app'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get my_rebus_app" do
+    get pages_my_rebus_app_url
+    assert_response :success
+  end
+
+end
